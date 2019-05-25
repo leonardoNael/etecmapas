a289989d933d0cc7394968eb7cbdca30ca728697
leonardoNael <leonardo.nael@hotmail.com>
2019-05-24 21:59:54 -0300

    Criação do projeto ETECMapas

---
+++ banco/banco.sql
@@ -0,0 +1,13 @@
+create database EtecMapas;
+use EtecMapas;
+create table etec
+	(id			bigint			not null		primary key		auto_increment,
+	 nome		varchar(50)		not null,
+     codigo		int				not null,
+     endereco	varchar(120)	not null,
+     bairro		varchar(60)		not null,
+     cidade		varchar(60)		not null,
+     cep		varchar(8)		not null,
+     latitude	double			not null,
+     longitude	double			not null);
+select * from etec
